b8298b2ecaf2843b5906e1b12b01bae8d174fe40
Isaac Pires <isaac.aragao.p@gmail.com>
2024-04-24 17:23:26 -0300

    .env added

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import { initializeApp, applicationDefault, cert } from 'firebase-admin/app'
 import express from 'express'
+require('dotenv').config()
 
 const serviceAccount = require("./secrete-key-firebase.json")
 initializeApp({
@@ -9,14 +10,14 @@ initializeApp({
 import {TestConn} from './source/db'
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  console.log(req.body)
+app.get('/NovoUsuario', (req, res) => {
   res.send(TestConn(req.body))
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
